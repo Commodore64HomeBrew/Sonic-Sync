2a0212be2170fd2fb28ad33498b3f2cd97f3d13a
Kevin Casteels <kcasteels@gmail.com>
2019-01-27 01:32:00 -0800

    Initial commit from Global Game Jam 2019

---
+++ src/main.c
@@ -0,0 +1,219 @@
+#include <stdio.h>
+#include <conio.h>
+#include <cc65.h>
+
+#include <peekpoke.h>
+
+#define KEY_F1  0x85
+#define KEY_F3  0x86
+
+#define RVSON  0x12
+#define RVSOFF 0x92
+
+#define CHAR_RAM 0x0400
+#define COLOUR_RAM 0xD800
+
+#define baseline 12
+
+unsigned char level = 1;
+
+short wave(unsigned char x){
+
+	unsigned char y;
+
+	if(level==1){
+		y = cc65_sin(x)+ baseline;
+		//gotoxy(1,2);
+		//printf("y:%d",y2);
+	}
+
+
+	//24 rows on screen
+	//12 per side
+	//12*2 = 24
+	//There are 24 half blocks per half column
+	//
+
+	//round();
+
+	return y;
+
+}
+
+
+int main(void) { 
+
+	//unsigned char block[8] = {0xA0, 0xBB, 0xAC	}
+	unsigned long time_step, time_now;
+	unsigned char x,xp1;
+	unsigned char y, yp1,y2;
+	unsigned char wx,wxp1;
+	unsigned char colour;
+	unsigned char step_size = 1;
+	unsigned short offset;
+
+	unsigned short deg;
+	
+
+	time_step=(PEEK(160)*65536)+(PEEK(161)*256+PEEK(162))+step_size;
+
+	offset = 0;
+	colour = 1;
+	x=0;
+	xp1=1;
+
+	POKE(0xC7 , 0x12);//Reverse on
+	bgcolor(0);
+
+	while(1){
+		//y=wave(x);
+		//yp1=wave(xp1);
+		textcolor(0);
+
+		//gotoxy(x-1,y);
+		//putchar(0xA0);
+		//0-360
+		xp1=xp1+2;
+		if(xp1>40){xp1=0;}
+		//deg=(xp1/40)*360;
+
+		deg=xp1*9;
+		if(deg>360){
+
+		}
+		y = cc65_sin(deg)/36 + baseline;
+		//gotoxy(20,20);
+		//printf("         ");
+		//printf("y:%d",y);
+		
+		textcolor(colour);
+		
+
+		gotoxy(x,y);
+		putchar(0xA0);
+		
+		POKE(0xC7 , 0);//Reverse off
+
+		//baseline x axis
+		gotoxy(x,baseline);
+		putchar(0xC0);
+		POKE(0xC7 , 0x12);//Reverse on
+
+		/*y2=y;
+		if(y>baseline){
+
+			//POKE(COLOUR_RAM + offset , colour);
+
+
+			gotoxy(x,y);
+			putchar(0xA0);
+			//y2= 
+			while((y2-baseline)>0){
+				gotoxy(1,1);
+				printf("y2:%d",y2);
+				gotoxy(x,y2);
+				putchar(0xA0);
+				//POKE(CHAR_RAM + offset , 0xA0);
+				--y2;
+
+			}
+
+		}
+		else{
+
+		}
+		*/
+			//wx = y % 2;
+
+			/*
+			if(wx==0){
+				if(wxp1==1){
+					//0xAC
+					POKE(CHAR_RAM + offset , 0xAC);
+					POKE(COLOUR_RAM + offset , colour);
+				}
+				else if (wxp1==2){
+					POKE(0xC7 , 0x12);//Reverse on
+					//REV
+					//0xE1
+					POKE(CHAR_RAM + offset , 0xE1);
+					POKE(COLOUR_RAM + offset , colour);
+					POKE(0xC7 , 0);//Reverse off
+
+				}
+				//0xA0
+				POKE(CHAR_RAM + offset , 0xA0);
+				POKE(COLOUR_RAM + offset , colour);
+			}
+			else if(wx==1){
+				if(wxp1==1){
+					//0xA2
+					POKE(CHAR_RAM + offset , 0xA2);
+					POKE(COLOUR_RAM + offset , colour);
+
+				}
+				else if (wxp1==2){
+					//REV
+					//0xBE
+					POKE(0xC7 , 0x12);//Reverse on
+					POKE(CHAR_RAM + offset , 0xBE);
+					POKE(COLOUR_RAM + offset , colour);
+					POKE(0xC7 , 0);//Reverse off
+				}
+				//0xBB
+				POKE(CHAR_RAM + offset , 0xBB);
+				POKE(COLOUR_RAM + offset , colour);
+			}
+			else if(wx==2){
+				if(wxp1==1){
+					//REV
+					//0xBC
+					POKE(0xC7 , 0x12);//Reverse on
+					POKE(CHAR_RAM + offset , 0xBC);
+					POKE(COLOUR_RAM + offset , colour);
+					POKE(0xC7 , 0);//Reverse off
+				}
+				else if(wxp1==2){
+					//REV
+					//0xA0
+					POKE(0xC7 , 0x12);//Reverse on
+					POKE(CHAR_RAM + offset , 0xA0);
+					POKE(COLOUR_RAM + offset , colour);
+					POKE(0xC7 , 0);//Reverse off
+				}
+				//0xA1
+				POKE(CHAR_RAM + offset , 0xA1);
+				POKE(COLOUR_RAM + offset , colour);	
+
+			}
+			*/
+		//}
+		/*offset = offset + 40;
+		if(offset>1000){offset=0;}
+		*/
+
+		//colour++;
+		//if(colour>15){colour=0;}
+
+
+
+		//putc(block[i]);
+
+
+		/*while(time_now<time_step){
+			time_now = (PEEK(160)*65536)+(PEEK(161)*256+PEEK(162));
+		}*/
+
+
+
+		x=x+1;
+		//xp1=x+1;
+		if(x>39){
+			x=0;
+			colour++;
+			if(colour>15){colour=0;}
+		}
+	}
+
+    return 0; 
+}
